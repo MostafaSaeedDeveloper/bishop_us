0ad8c6763693f17a67858dc8781e1d185281ee2e
MostafaSaeedDeveloper <mostafasaeed.developer@gmail.com>
2026-02-25 00:42:49 +0200

    hello

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,185 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en" class="remember-theme">
+  <head>
+    <meta charset="utf-8">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <title>Bishop Dashboard Login</title>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <meta name="description" content="Dashmix - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="index, follow">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Open Graph Meta -->
+    <meta property="og:title" content="Dashmix - Bootstrap 5 Admin Template &amp; UI Framework">
+    <meta property="og:site_name" content="Dashmix">
+    <meta property="og:description" content="Dashmix - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+    <!-- END Icons -->
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+    <!-- Stylesheets -->
+    <!-- Dashmix framework -->
+    <link rel="stylesheet" id="css-main" href="{{asset('admin/assets/css/dashmix.min.css')}}">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/xwork.min.css"> -->
+    <!-- END Stylesheets -->
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Load and set color theme + dark mode preference (blocking script to prevent flashing) -->
+    <script src="assets/js/setTheme.js"></script>
+  </head>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+  <body>
+    <!-- Page Container -->
+    <!--
+      Available classes for #page-container:
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+      SIDEBAR & SIDE OVERLAY
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+        'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+        'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+        'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+        'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+        'sidebar-dark'                              Dark themed sidebar
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+        'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+        'side-overlay-o'                            Visible Side Overlay by default
+
+        'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+        'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+      HEADER
+
+        ''                                          Static Header if no class is added
+        'page-header-fixed'                         Fixed Header
+
+
+      FOOTER
+
+        ''                                          Static Footer if no class is added
+        'page-footer-fixed'                         Fixed Footer (please have in mind that the footer has a specific height when is fixed)
+
+      HEADER STYLE
+
+        ''                                          Classic Header style if no class is added
+        'page-header-dark'                          Dark themed Header
+        'page-header-glass'                         Light themed Header with transparency by default
+                                                    (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+        'page-header-glass page-header-dark'         Dark themed Header with transparency by default
+                                                    (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+      MAIN CONTENT LAYOUT
+
+        ''                                          Full width Main Content if no class is added
+        'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+        'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+    -->
+    <div id="page-container">
+
+      <!-- Main Container -->
+      <main id="main-container">
+        <!-- Page Content -->
+        <div class="bg-image" style="background-image: url('assets/media/photos/photo19@2x.jpg');">
+          <div class="row g-0 justify-content-center bg-primary-dark-op">
+            <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
+              <!-- Sign In Block -->
+              <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
+                <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
+                  <!-- Header -->
+                  <div class="mb-2 text-center">
+                    <a class="link-fx fw-bold fs-1" href="index.html">
+                      <span class="text-dark">Dash</span><span class="text-primary">mix</span>
+                    </a>
+                    <p class="text-uppercase fw-bold fs-sm text-muted">Sign In</p>
+                  </div>
+                  <!-- END Header -->
+
+                  <!-- Sign In Form -->
+                  <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _js/pages/op_auth_signin.js) -->
+                  <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                  <form class="js-validation-signin" action="be_pages_auth_all.html" method="POST">
+                    <div class="mb-4">
+                      <div class="input-group input-group-lg">
+                        <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Username">
+                        <span class="input-group-text">
+                          <i class="fa fa-user-circle"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="mb-4">
+                      <div class="input-group input-group-lg">
+                        <input type="password" class="form-control" id="login-password" name="login-password" placeholder="Password">
+                        <span class="input-group-text">
+                          <i class="fa fa-asterisk"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="d-sm-flex justify-content-sm-between align-items-sm-center text-center text-sm-start mb-4">
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="login-remember-me" name="login-remember-me" checked>
+                        <label class="form-check-label" for="login-remember-me">Remember Me</label>
+                      </div>
+                      <div class="fw-semibold fs-sm py-1">
+                        <a href="javascript:void(0)">Forgot Password?</a>
+                      </div>
+                    </div>
+                    <div class="text-center mb-4">
+                      <button type="submit" class="btn btn-hero btn-primary">
+                        <i class="fa fa-fw fa-sign-in-alt opacity-50 me-1"></i> Sign In
+                      </button>
+                    </div>
+                  </form>
+                  <!-- END Sign In Form -->
                 </div>
+                <div class="block-content bg-body">
+                  <div class="d-flex justify-content-center text-center push">
+                    <a class="item item-circle item-tiny me-1 bg-default" data-toggle="theme" data-theme="default" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xwork" data-toggle="theme" data-theme="assets/css/themes/xwork.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xmodern" data-toggle="theme" data-theme="assets/css/themes/xmodern.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xeco" data-toggle="theme" data-theme="assets/css/themes/xeco.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xsmooth" data-toggle="theme" data-theme="assets/css/themes/xsmooth.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xinspire" data-toggle="theme" data-theme="assets/css/themes/xinspire.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xdream" data-toggle="theme" data-theme="assets/css/themes/xdream.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny me-1 bg-xpro" data-toggle="theme" data-theme="assets/css/themes/xpro.min.css" href="#"></a>
+                    <a class="item item-circle item-tiny bg-xplay" data-toggle="theme" data-theme="assets/css/themes/xplay.min.css" href="#"></a>
+                  </div>
+                </div>
+              </div>
+              <!-- END Sign In Block -->
             </div>
+          </div>
         </div>
+        <!-- END Page Content -->
+      </main>
+      <!-- END Main Container -->
     </div>
-</div>
-@endsection
+    <!-- END Page Container -->
+
+    <!--
+      Dashmix JS
+
+      Core libraries and functionality
+      webpack is putting everything together at assets/_js/main/app.js
+    -->
+    <script src="{{asset('admin/assets/js/dashmix.app.min.js')}}"></script>
+
+    <!-- jQuery (required for jQuery Validation plugin) -->
+    <script src="{{asset('admin/assets/js/lib/jquery.min.js')}}"></script>
+
+    <!-- Page JS Plugins -->
+    <script src="{{asset('admin/assets/js/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+
+    <!-- Page JS Code -->
+    <script src="{{asset('admin/assets/js/pages/op_auth_signin.min.js')}}"></script>
+  </body>
+</html>
