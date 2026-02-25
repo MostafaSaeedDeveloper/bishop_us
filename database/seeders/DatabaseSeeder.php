049593f358188ff17bbcf6bef713ee2e76417d88
MostafaSaeedDeveloper <mostafasaeed.developer@gmail.com>
2026-02-25 18:34:16 +0200

    Stop downloading project images and keep filenames only

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('Bishop@2030'),
             ]
         );
+
+        $this->call(ProjectsFromBishopSeeder::class);
     }
 }
